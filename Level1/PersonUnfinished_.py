b2b48817986d14bba58aa5e39456b646108d57c4
HyeonJunOh <znzn2260@naver.com>
2021-09-08 15:25:34 +0900

    Update PersonUnfinished_.py
    완주하지 못한 선수

--- Level1/PersonUnfinished_.py
+++ Level1/PersonUnfinished_.py
@@ -1,18 +1,15 @@
+from collections import Counter
+# 내 풀이
 def solution(participant, completion):
-    p = sorted(participant)
+    participant = Counter(participant)
     for i in completion:
-        left, right = 0, len(p)-1
-        while left<=right:
-            mid = (left+right)//2
-            if p[mid] == i:
-                p.remove(i)
-                break
-            elif p[mid] < i:
-                left = mid + 1
-            else:
-                right = mid - 1
-        if left > right:
-            return i
-    return p[0]
+        if i in participant:
+            participant[i] -= 1
+    for k, v in participant.items():
+        if v == 1:
+            return k
 
+# 다른 사람 풀이
+# def solution(participant, completion):
+#     return list((Counter(participant) - Counter(completion)).keys())[0]
 print(solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"]))
