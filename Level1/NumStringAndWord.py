4d2c766b0f2c79cf45d714dafaa159e0eeaf8186
HyeonJunOh <znzn2260@naver.com>
2021-09-08 10:22:01 +0900

    Create NumStringAndWord.py
    숫자 문자열과 영단어

---
+++ Level1/NumStringAndWord.py
@@ -0,0 +1,5 @@
+def solution(s):
+    word = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for i, j in enumerate(word):
+        s = s.replace(j, str(i))
+    return int(s)
