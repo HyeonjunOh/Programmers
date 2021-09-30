56bedba465253b897fac5da154cb50a97e1977c4
HyeonJunOh <znzn2260@naver.com>
2021-09-30 20:37:34 +0900

    Update GymSuit.py
    체육복

--- Level1/GymSuit.py
+++ Level1/GymSuit.py
@@ -1,9 +1,15 @@
 def solution(n, lost, reserve):
-    answer = 0
-    tmp = []
-    for i in lost:
-        if i-1 in reserve:
-            tmp.append()
-    return answer
+    s = [1 if (i in reserve and i in lost) else 2 if i in reserve else 0 if i in lost else 1 for i in range(1, n+1)]
+    for i in range(n):
+        if i != 0 and s[i] == 2 and s[i-1] == 0:
+            s[i] -= 1
+            s[i-1] += 1
+        elif i != n-1 and s[i] == 2 and s[i+1] == 0:
+            s[i] -= 1
+            s[i+1] += 1
+    return n - s.count(0)
 
-print(solution(5, [2, 4], [1, 3, 5]))
+
+print(solution(30, [2, 4], [1, 3, 5]))
+print(solution(30, [2, 4], [3]))
+print(solution(30, [3], [1, 5]))
