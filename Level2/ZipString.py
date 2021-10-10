69b6ddd22f8f7d8e53de08f701f01d3ea0a772dd
HyeonJunOh <znzn2260@naver.com>
2021-10-10 19:02:21 +0900

    Update ZipString.py
    124 나라의 숫자

--- Level2/ZipString.py
+++ Level2/ZipString.py
@@ -1,19 +1,34 @@
+# def solution(s):
+#     answer = []
+#     for i in range(len(s)):
+#         spl = [s[j:j+i+1] for j in range(0, len(s), i+1)]
+#         st = ""
+#         pre, count = "", 0
+#         for idx, j in enumerate(spl):
+#             if pre != j:
+#                 st += [str(count) + pre, pre][count < 2]
+#                 pre = j
+#                 count = 1
+#             else:
+#                 count += 1
+#             if idx == len(spl)-1:
+#                 st += [str(count) + pre, pre][count < 2]
+#         answer.append(len(st))
+#     return min(answer)
+
 def solution(s):
     answer = []
     for i in range(len(s)):
         spl = [s[j:j+i+1] for j in range(0, len(s), i+1)]
-        st = ""
-        pre, count = "", 0
-        for idx, j in enumerate(spl):
-            if pre != j:
-                st += [str(count) + pre, pre][count < 2]
-                pre = j
-                count = 1
+        result = []
+        cnt = 1
+        for a, b in zip(spl, spl[1:]+[" "]):
+            if a == b:
+                cnt += 1
             else:
-                count += 1
-            if idx == len(spl)-1:
-                st += [str(count) + pre, pre][count < 2]
-        answer.append(len(st))
+                result.append([a, cnt])
+                cnt = 1
+        answer.append(sum([[len(a)+len(str(b)), len(a)][b < 2]for a, b in result]))
     return min(answer)
 
 #
